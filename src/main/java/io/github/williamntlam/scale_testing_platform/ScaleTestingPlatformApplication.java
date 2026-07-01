caa09a6bcb2f8a14fb85cbd15888dd33b3198a39
William Lam <williamntlam@gmail.com>
2026-06-30 21:05:47 -0400

    feat: initialize project structure

---
+++ src/main/java/io/github/williamntlam/scale_testing_platform/ScaleTestingPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.williamntlam.scale_testing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaleTestingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaleTestingPlatformApplication.class, args);
+	}
+
+}
