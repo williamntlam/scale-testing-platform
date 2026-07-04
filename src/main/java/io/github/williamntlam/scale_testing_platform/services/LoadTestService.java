37cd27f0aee2a589e03aa74a762538f4f8db0327
William Lam <williamntlam@gmail.com>
2026-07-03 22:02:06 -0400

    feat: implement virtual thread system w/ semaphores, fan in architecture with one virtual thread per api call

--- src/main/java/io/github/williamntlam/scale_testing_platform/services/LoadTestService.java
+++ src/main/java/io/github/williamntlam/scale_testing_platform/services/LoadTestService.java
@@ -1,11 +1,19 @@
 package io.github.williamntlam.scale_testing_platform.services;
 
 import java.net.http.HttpClient;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 import org.springframework.stereotype.Service;
 
 import io.github.williamntlam.scale_testing_platform.model.LoadTestRequest;
 import io.github.williamntlam.scale_testing_platform.model.LoadTestResponse;
+import io.github.williamntlam.scale_testing_platform.model.TestResponse;
+import io.github.williamntlam.scale_testing_platform.model.enums.TestStatus;
 
 @Service
 public class LoadTestService {
@@ -18,7 +26,41 @@ public class LoadTestService {
     }
 
     public LoadTestResponse run(LoadTestRequest request) throws InterruptedException {
-        throw new UnsupportedOperationException("Not yet implemented");
+        List<String> payloads = request.payloads();
+        int totalTasks = payloads.size();
+
+        AtomicReferenceArray<TestResponse> results = new AtomicReferenceArray<>(totalTasks);
+        CountDownLatch done = new CountDownLatch(totalTasks);
+        Semaphore inFlight = new Semaphore(request.concurrencyLimit());
+
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int index = 0; index <= totalTasks - 1; index++) {
+
+                final int taskId = index;
+                final String payload = payloads.get(index);
+
+                inFlight.acquire();
+
+                executor.submit(() -> {
+                    try {
+                        TestResponse result = executeTask(taskId, request.targetUri(), payload);
+                        results.set(taskId, result);
+                    } catch (Exception e) {
+                        results.set(taskId, new TestResponse(taskId, TestStatus.FAILED, e.getMessage()));
+                    } finally {
+                        inFlight.release();
+                        done.countDown();
+                    }
+                });
+
+            }
+        
+            done.await();
+        
+        } 
+
+        return aggregate(results);
+
     }
 
 }
