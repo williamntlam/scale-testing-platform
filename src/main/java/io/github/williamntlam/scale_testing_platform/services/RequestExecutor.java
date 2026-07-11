c89cfeb6a60b22978cb810d0198175bae25008b8
William Lam <williamntlam@gmail.com>
2026-07-11 17:47:52 -0400

    feat: implement request executor interface and record for outbound response

--- src/main/java/io/github/williamntlam/scale_testing_platform/services/RequestExecutor.java
+++ src/main/java/io/github/williamntlam/scale_testing_platform/services/RequestExecutor.java
@@ -3,5 +3,5 @@ package io.github.williamntlam.scale_testing_platform.services;
 import java.net.URI;
 
 public interface RequestExecutor {
-    OutboundResponse send(URI targetUri, String payload) throws Exception;
+  OutboundResponse send(URI targetUri, String payload) throws Exception;
 }
