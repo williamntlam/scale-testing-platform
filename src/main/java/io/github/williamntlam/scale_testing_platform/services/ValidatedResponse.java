a52d07da9cb335c456a05040383dd5e95f8d1ee8
William Lam <williamntlam@gmail.com>
2026-07-13 21:43:14 -0400

    feat: implement ValidatedResponse.java

--- src/main/java/io/github/williamntlam/scale_testing_platform/services/ValidatedResponse.java
+++ src/main/java/io/github/williamntlam/scale_testing_platform/services/ValidatedResponse.java
@@ -2,8 +2,4 @@ package io.github.williamntlam.scale_testing_platform.services;
 
 import io.github.williamntlam.scale_testing_platform.model.enums.TestStatus;
 
-public record ValidatedResponse(
-    TestStatus status,
-    String safeBody, 
-    int originalByteCount
-) {}
+public record ValidatedResponse(TestStatus status, String safeBody, int originalByteCount) {}
